a5e9ed0c7fa9276bf4d8e9be4ba5b51e9bf5d97e
SH-LIMA <haliamsaid.pro@gmail.com>
2021-03-12 11:57:19 +0100

    add htmlspecialchart

--- config/sendmsg.php
+++ config/sendmsg.php
@@ -5,7 +5,7 @@ if(isset($_POST["message"])){
 //   echo $_SESSION['admin']; 
   $user_online = read($_SESSION['admin']);
 //   var_dump($user_online["id"] );
-
-  insert_msg($_POST["message"],$user_online["id"]);
+    $messageConvert = htmlspecialchars($_POST["message"], ENT_QUOTES);
+  insert_msg($messageConvert,$user_online["id"]);
   
 }
